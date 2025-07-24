8634655a6e4dc7ae8336ff11f4cc291bd60f9b51
Nhật Hưng <hung2004py@gmail.com>
2025-07-24 13:54:52 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export FLASK_APP=server/run.py
-echo "Running database migrations..."
-python -m flask db upgrade
-echo "Starting gunicorn..."
-python -m gunicorn server.run:app -b 0.0.0.0:$PORT --timeout 120 --workers 1
