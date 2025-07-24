b86b9cc073b7368fdb4097ff23a370b4d05484b2
Nhật Hưng <hung2004py@gmail.com>
2025-07-24 13:51:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=server/run.py
 echo "Running database migrations..."
-python3 -m flask db upgrade
+python -m flask db upgrade
 echo "Starting gunicorn..."
-python3 -m gunicorn server.run:app -b 0.0.0.0:$PORT --timeout 120 --workers 1
+python -m gunicorn server.run:app -b 0.0.0.0:$PORT --timeout 120 --workers 1
