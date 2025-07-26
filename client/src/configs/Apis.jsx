88ebf5165882fa71650a231147282796eab4c61f
LilLoi <loinguyen.310704@gmail.com>
2025-07-26 15:52:16 +0700

    fix api

--- client/src/configs/Apis.jsx
+++ client/src/configs/Apis.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import cookie from "react-cookies";
 
-const BASE_URL = "http://127.0.0.1:5000/api/";
+const BASE_URL = "https://thuvienso-production.up.railway.app/api/";
 
 export const authApis = () => {
   return axios.create({
